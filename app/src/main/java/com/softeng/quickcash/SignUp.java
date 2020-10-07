bf05b7805d6aae25734f6c1c551b40999a62ce74
alrawhani <md961867@dal.ca>
2020-10-06 21:55:47 -0300

    feature: US-4, UI

---
+++ app/src/main/java/com/softeng/quickcash/SignUp.java
@@ -0,0 +1,21 @@
+package com.softeng.quickcash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void createAccountOnButtonClick(View view) {
+
+
+    }
+}
