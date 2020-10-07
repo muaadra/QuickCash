a18c9f7d40f075679364a6138516814935272b1a
alrawhani <md961867@dal.ca>
2020-10-06 22:58:03 -0300

    feature: US-4

--- app/src/main/java/com/softeng/quickcash/SignUp.java
+++ app/src/main/java/com/softeng/quickcash/SignUp.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
@@ -14,8 +15,28 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
     }
 
+    /**
+     * this method is called when user clicks on the registration button
+     */
     public void createAccountOnButtonClick(View view) {
+        //reset warning labels/text views
+        clearWarningLabels();
 
+        InputValidation validation = new InputValidation();
 
+        String email = ((EditText) findViewById(R.id.email_EditText))
+                .getText().toString();
+
+        //validate email input
+        if(!validation.isEmailValid(email)){
+            ((TextView) findViewById(R.id.emailStatus))
+                    .setText(R.string.InvalidEmail);
+        }
+
+    }
+
+    private void clearWarningLabels(){
+        ((TextView) findViewById(R.id.emailStatus)).setText("");
+        ((TextView) findViewById(R.id.passwordStatus)).setText("");
     }
 }
