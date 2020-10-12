0db3c872d2692fa94ee8e530acc487b91a3bc226
hpark <hn946002@dal.ca>
2020-10-12 01:33:11 -0300

    AT2-1 test  : user credential check.

---
+++ app/src/androidTest/java/com/softeng/quickcash/SignInTest.java
@@ -0,0 +1,86 @@
+package com.softeng.quickcash;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SignInTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+    static FirebaseDatabase database = null;
+    static DatabaseReference itemRef = null;
+    static Map<String, HashMap<String, HashMap<String, HashMap<String, String>>>> info;
+
+    @Before
+    public void setup() {
+        onView(withId(R.id.signInButton)).perform(click());
+        onView(withId(R.id.SignInActivity_Layout)).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Build : Builds a database for a firebase test.
+     */
+    @BeforeClass
+    public static void build() {
+        database = FirebaseDatabase.getInstance();
+        itemRef = database.getReference();
+        itemRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                info = (Map<String, HashMap<String, HashMap<String, HashMap<String, String>>>>) dataSnapshot.getValue();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    /**
+     * AT2-1 : Check correct credential
+     */
+    @Test
+    public void test_check_user_correct_credential() {
+        HashMap<String, String> signUpInfo = new HashMap<String, String>();
+        signUpInfo.put("email", "signUp@test.com");
+        signUpInfo.put("password", "testPassword");
+        onView(withId(R.id.input_email_address)).perform(typeText("signUp@test;com"));
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.input_password)).perform(typeText("testPassword"));
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.btn_login)).perform(click());
+        assertTrue(signUpInfo.equals(info.get("users").get("signUp@test;com").get("SignUpInfo")));
+    }
+
+
+}
--- app/src/main/java/com/softeng/quickcash/MainActivity.java
+++ app/src/main/java/com/softeng/quickcash/MainActivity.java
@@ -18,4 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
     }
+    public void signIn(View view) {
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
+    }
 }
