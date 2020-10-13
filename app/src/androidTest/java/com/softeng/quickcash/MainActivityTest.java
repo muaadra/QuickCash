45187848e1f08d692e5d83f588c25b759023fc0f
alrawhani <md961867@dal.ca>
2020-10-13 20:38:42 -0300

    espresso test: US-4

--- app/src/androidTest/java/com/softeng/quickcash/MainActivityTest.java
+++ app/src/androidTest/java/com/softeng/quickcash/MainActivityTest.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.After;
@@ -14,9 +16,13 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 
 public class MainActivityTest {
@@ -71,6 +77,32 @@ public class MainActivityTest {
         onView(withId(R.id.welcomeLayout)).check(doesNotExist());
 
     }
+
+    /**
+     * testing sign out button
+     */
+    @Test
+    public void signOutTest(){
+        //setup, making sure user is signed in
+        activityScenarioRule.getScenario().onActivity(
+                new ActivityScenario.ActivityAction<MainActivity>() {
+                    @Override
+                    public void perform(MainActivity activity) {
+                        UserStatusData.removeAllUserPreferences(activity);
+                        UserStatusData.saveUserData("email","jojo@mo.com", activity);
+                        //restarting activity
+                        Intent intent = new Intent(activity, MainActivity.class);
+                        activity.startActivity(intent);
+                    }
+                });
+
+        onView(withId(R.id.signOut)).perform(click());
+
+        onView(withId(R.id.emailStatus))
+                .check(matches(withText(R.string.YouAreSignedOut)));
+
+    }
+
 
     /**
      * clear all data from SharedPreferences
