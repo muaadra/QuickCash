d87b5738fd15a9e1ecfbc92648ff396c61115d9d
hpark <hn946002@dal.ca>
2020-10-13 17:38:19 -0300

    refector:US-2

--- app/src/androidTest/java/com/softeng/quickcash/SignInActivityTest.java
+++ app/src/androidTest/java/com/softeng/quickcash/SignInActivityTest.java
@@ -1,47 +1,31 @@
 package com.softeng.quickcash;
 
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressBack;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.endsWith;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class SignInActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
-    static FirebaseDatabase database = null;
-    static DatabaseReference itemRef = null;
-    static Map<String, HashMap<String, HashMap<String, HashMap<String, String>>>> info;
 
     @Before
     public void setup() {
@@ -50,30 +34,10 @@ public class SignInActivityTest {
     }
 
     /**
-     * Build : Builds a database for a firebase test.
-     */
-    @BeforeClass
-    public static void build() {
-        database = FirebaseDatabase.getInstance();
-        itemRef = database.getReference();
-        itemRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                info = (Map<String, HashMap<String, HashMap<String, HashMap<String, String>>>>) dataSnapshot.getValue();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    /**
-     * AT2-1 : Check correct credential
+     * this UI test checks if email entered is valid
      */
     @Test
-    public void test_check_user_correct_credential() {
+    public void test_user_correct_credential() {
         HashMap<String, String> signUpInfo = new HashMap<String, String>();
         signUpInfo.put("email", "signUp@test.com");
         signUpInfo.put("password", "testPassword");
@@ -83,15 +47,32 @@ public class SignInActivityTest {
         onView(withId(R.id.input_password)).perform(typeText("testPassword"));
         onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
-        assertTrue(signUpInfo.equals(info.get("users").get("signUp@test;com").get("SignUpInfo")));
         onView(withId(R.id.msg_confirm)).check(matches(isDisplayed()));
     }
 
     /**
-     * AT2-3 : Check wrong credentials
+     * this Junit test if email and password are valid
      */
     @Test
-    public void test_check_user_wrong_credential(){
+    public void test_user_input() {
+        HashMap<String, String> signUpInfo = new HashMap<String, String>();
+        signUpInfo.put("email", "signUp@test.com");
+        signUpInfo.put("password", "testPassword");
+
+        InputValidation input = new InputValidation();
+        assertTrue(input.isEmailValid(signUpInfo.get("email")));
+        assertTrue(input.isEmailFormatValid(signUpInfo.get("email")));
+        assertTrue(input.isEmailValid(signUpInfo.get("email")));
+        assertTrue(input.isPasswordCaseValid(signUpInfo.get("password")));
+        assertTrue(input.isPasswordLengthValid(signUpInfo.get("password")));
+        assertTrue(input.isPasswordValid(signUpInfo.get("password")));
+    }
+
+    /**
+     * this Ui test if entered password is invalid.
+     */
+    @Test
+    public void test_user_wrong_credential() {
         HashMap<String, String> signUpInfo = new HashMap<String, String>();
         signUpInfo.put("email", "signUp@test.com");
         signUpInfo.put("password", "testPassword1111");   //Wrong password
@@ -102,8 +83,28 @@ public class SignInActivityTest {
         onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
 
-        assertFalse(signUpInfo.equals(info.get("users").get("signUp@test;com").get("SignUpInfo")));
         onView(withId(R.id.err_msg_pw)).check(matches(isDisplayed()));
     }
+
+    /**
+     * this Ui test if user not found.
+     */
+    @Test
+    public void test_user_not_found() {
+        HashMap<String, String> signUpInfo = new HashMap<String, String>();
+        signUpInfo.put("email", "signUp@test.com");
+        signUpInfo.put("password", "testPassword");
+        onView(withId(R.id.input_email)).perform(typeText("signUp@test.com"));
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.input_password)).perform(typeText("testPassword"));
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.btn_login)).perform(click());
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.err_msg_email)).check(matches(isDisplayed()));
+    }
+
 
 }
