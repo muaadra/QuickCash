0d410f6e5f37e571e6c07233544213cef0f59bb1
alrawhani <md961867@dal.ca>
2020-10-13 23:40:24 -0300

    "espresso test: US-3"

--- app/src/androidTest/java/com/softeng/quickcash/CreateProfileTest.java
+++ app/src/androidTest/java/com/softeng/quickcash/CreateProfileTest.java
@@ -1,6 +1,9 @@
 package com.softeng.quickcash;
 
 
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
@@ -17,6 +20,7 @@ import org.junit.Test;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -35,16 +39,34 @@ public class CreateProfileTest {
 
             onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
 
-            // onView(withId(R.id.editProfileAboutMe)).perform(click()).perform(typeText("This is about me"));
-
-            //  onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
-
             onView(withId(R.id.createProfile)).perform(click());
 
             onView(withId(R.id.textViewProfileConfirm)).check(matches(withText(R.string.profileCreated)));
 
         }
 
+    /**
+     * tests that the main activity is showing after user canceled creating a profile
+     */
+    @Test
+    public void welcomeActivityNotShowingAfterFirstRunTest(){
+        //setup, making sure user is signed in
+        activityScenarioRule.getScenario().onActivity(
+                new ActivityScenario.ActivityAction<CreateProfile>() {
+                    @Override
+                    public void perform(CreateProfile activity) {
+                        UserStatusData.removeAllUserPreferences(activity);
+                        UserStatusData.saveUserData("email","jojo@mo.com", activity);
+                    }
+                });
 
+        onView(withId(R.id.cancelProfile)).perform(click());
+
+        //check if screen is displayed
+        onView(withId(R.id.mainActivityLayOut))
+                .check(ViewAssertions.matches(isDisplayed()));
     }
+
+
+}
 
