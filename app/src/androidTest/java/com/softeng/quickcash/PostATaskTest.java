cd342674e438a09c5b7dacb2d3a09a3b02aaa47e
alrawhani <md961867@dal.ca>
2020-10-23 14:59:52 -0300

    espresso test: US-6

--- app/src/androidTest/java/com/softeng/quickcash/PostATaskTest.java
+++ app/src/androidTest/java/com/softeng/quickcash/PostATaskTest.java
@@ -5,6 +5,8 @@ import android.widget.Spinner;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Rule;
@@ -12,15 +14,21 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
@@ -31,7 +39,7 @@ public class PostATaskTest {
             = new ActivityScenarioRule<>(PostATaskActivity.class);
 
     @Test
-    public void PostATaskActivityNotShowingTest(){
+    public void PostATaskActivityNotShowingTest() {
 
         onView(withId(R.id.datePickerPostAtask)).perform(click());
 
@@ -40,13 +48,13 @@ public class PostATaskTest {
     }
 
     @Test
-    public void taskTypesSpinnerIsNotEmpty_Test(){
+    public void taskTypesSpinnerIsNotEmpty_Test() {
         final int[] count = {-1};
         activityScenarioRule.getScenario().onActivity(
                 new ActivityScenario.ActivityAction<PostATaskActivity>() {
                     @Override
                     public void perform(PostATaskActivity activity) {
-                        count[0] =((Spinner)activity.findViewById(R.id.tasksTypeSpinner_PostATask)).getCount();
+                        count[0] = ((Spinner) activity.findViewById(R.id.tasksTypeSpinner_PostATask)).getCount();
                     }
                 });
 
@@ -54,7 +62,7 @@ public class PostATaskTest {
     }
 
     @Test
-    public void taskTypesSpinnerHasSameCount_Test(){
+    public void taskTypesSpinnerHasSameCount_Test() {
         final int[] count = {-1};
         final List<String>[] taskTypes = new List[]{new ArrayList<>()};
 
@@ -63,19 +71,40 @@ public class PostATaskTest {
                     @Override
                     public void perform(PostATaskActivity activity) {
                         taskTypes[0] = Arrays.asList(activity.taskTypes);
-                        count[0] =((Spinner)activity.findViewById(R.id.tasksTypeSpinner_PostATask)).getCount();
+                        count[0] = ((Spinner) activity.findViewById(R.id.tasksTypeSpinner_PostATask)).getCount();
                     }
                 });
 
-        assertEquals(taskTypes[0].size(),count[0]);
+        assertEquals(taskTypes[0].size(), count[0]);
     }
 
     @Test
-    public void ExpectedDateFieldIsNotEmptyWhenSubmit_Test(){
+    public void ExpectedDateFieldIsNotEmptyWhenSubmit_Test() {
+
+        activityScenarioRule.getScenario().onActivity(
+                new ActivityScenario.ActivityAction<PostATaskActivity>() {
+                    @Override
+                    public void perform(PostATaskActivity activity) {
+                        activity.taskTypes[1] = "Task 1";
+                        activity.spinnerSetup();
+                    }
+                });
+
+        onView(withId(R.id.tasksTypeSpinner_PostATask)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Task 1"))).perform(click());
+
+        onView(withId(R.id.taskDescEditTxt)).perform(typeText("my description"));
+
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.costEditTxt)).perform(click())
+                .perform(typeText("30"));
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+
         onView(withId(R.id.postATaskButton)).perform(click());
 
         onView(withId(R.id.postATaskStatus))
-                .check(matches(withText(R.string.ExpectedDateError)));;
+                .check(matches(withText(R.string.ExpectedDateError)));
 
     }
 }
