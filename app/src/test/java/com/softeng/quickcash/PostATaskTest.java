5849d2547937a6f6d3d7234ef588ab0ef83d643f
alrawhani <md961867@dal.ca>
2020-10-22 21:58:48 -0300

    junit test: US-6

---
+++ app/src/test/java/com/softeng/quickcash/PostATaskTest.java
@@ -0,0 +1,16 @@
+package com.softeng.quickcash;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PostATaskTest {
+
+    @Test
+    public void checkIfAStringIsEmpty_Test1() {
+        PostATaskActivity postATask = new PostATaskActivity();
+        assertTrue(postATask.isStringEmpty(""));
+    }
+
+}
