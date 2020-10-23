b2536b00f8dc9d3a601592e46305f041891ffe3d
alrawhani <md961867@dal.ca>
2020-10-22 22:38:31 -0300

    feature: US-6

--- app/src/main/java/com/softeng/quickcash/MyPosts.java
+++ app/src/main/java/com/softeng/quickcash/MyPosts.java
@@ -1,8 +1,16 @@
 package com.softeng.quickcash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.ArrayList;
 
 public class MyPosts extends AppCompatActivity {
 
@@ -11,6 +19,16 @@ public class MyPosts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_posts);
     }
+
+    /**
+     * runs when "post a task" button is clicked
+     */
+    public void goToPostATaskOnClickButton(View view) {
+        //go to next activity
+        Intent intent = new Intent(this, PostATaskActivity.class);
+        startActivity(intent);
+    }
+
 
 
 }
--- app/src/main/java/com/softeng/quickcash/PostATaskActivity.java
+++ app/src/main/java/com/softeng/quickcash/PostATaskActivity.java
@@ -1,10 +1,18 @@
 package com.softeng.quickcash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PostATaskActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class PostATaskActivity extends AppCompatActivity  implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +21,29 @@ public class PostATaskActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * runs when calendar button is clicked
+     */
+    public void openCalenderOnClickButton(View view) {
+        DialogFragment datePicker = new DatePicker();
+        datePicker.show(getSupportFragmentManager(),"date picker");
+    }
+
     public boolean isStringEmpty(String text){
         if(text == null || text.equals("")){
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onDateSet(android.widget.DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.YEAR,year);
+        calendar.set(calendar.MONTH,month);
+        calendar.set(calendar.DAY_OF_MONTH,dayOfMonth);
+
+        String date = DateFormat.getDateInstance().format(calendar.getTime());
+        ((Button)findViewById(R.id.datePickerPostAtask)).setText(date);
     }
 }
