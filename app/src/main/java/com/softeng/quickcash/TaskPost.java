2bb781add68243d3f898b055dd4c0870d27b806e
alrawhani <md961867@dal.ca>
2020-10-23 17:32:40 -0300

    feature: US-6

--- app/src/main/java/com/softeng/quickcash/TaskPost.java
+++ app/src/main/java/com/softeng/quickcash/TaskPost.java
@@ -1,17 +1,30 @@
 package com.softeng.quickcash;
 
+import java.util.Date;
+
 public class TaskPost {
     private String taskTitle;
     private String taskDescription;
     private String taskCost;
+    private boolean isPostClosed;
+    private boolean isPostAccepted;
+    private Date expectedDate;
+    private String latLonLocation;
 
     public TaskPost() {
     }
 
-    public TaskPost(String taskTitle, String taskDescription, String taskCost) {
+    public TaskPost(String taskTitle, String taskDescription,
+                    String taskCost, boolean isPostClosed,
+                    boolean isPostAccepted, Date expectedDate,
+                    String latLonLocation) {
         this.taskTitle = taskTitle;
         this.taskDescription = taskDescription;
         this.taskCost = taskCost;
+        this.isPostClosed = isPostClosed;
+        this.isPostAccepted = isPostAccepted;
+        this.expectedDate = expectedDate;
+        this.latLonLocation = latLonLocation;
     }
 
     public String getTaskTitle() {
@@ -36,5 +49,37 @@ public class TaskPost {
 
     public void setTaskCost(String taskCost) {
         this.taskCost = taskCost;
+    }
+
+    public boolean isPostClosed() {
+        return isPostClosed;
+    }
+
+    public void setPostClosed(boolean postClosed) {
+        isPostClosed = postClosed;
+    }
+
+    public boolean isPostAccepted() {
+        return isPostAccepted;
+    }
+
+    public void setPostAccepted(boolean postAccepted) {
+        isPostAccepted = postAccepted;
+    }
+
+    public Date getExpectedDate() {
+        return expectedDate;
+    }
+
+    public void setExpectedDate(Date expectedDate) {
+        this.expectedDate = expectedDate;
+    }
+
+    public String getLatLonLocation() {
+        return latLonLocation;
+    }
+
+    public void setLatLonLocation(String latLonLocation) {
+        this.latLonLocation = latLonLocation;
     }
 }
