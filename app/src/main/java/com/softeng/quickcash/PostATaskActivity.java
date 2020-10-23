2a95b4c92a808f72027ce80256b8aaf68673f197
alrawhani <md961867@dal.ca>
2020-10-23 16:54:01 -0300

    bug fix: US-6

--- app/src/main/java/com/softeng/quickcash/PostATaskActivity.java
+++ app/src/main/java/com/softeng/quickcash/PostATaskActivity.java
@@ -1,25 +1,34 @@
 package com.softeng.quickcash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
-public class PostATaskActivity extends AppCompatActivity
-        implements DatePickerDialog.OnDateSetListener {
+public class PostATaskActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    MyLocation myLocation;
 
     public String[] taskTypes = {"Select A Category"
             , "Task1", "Task2","Task3","Task4"};
@@ -33,9 +42,39 @@ public class PostATaskActivity extends AppCompatActivity
         String selectACategory = getResources().getString(R.string.selectATask);
         taskTypes[0] = selectACategory;
 
+        getLocation();
         spinnerSetup();
     }
 
+
+    private void getLocation(){
+        myLocation = new MyLocation(this) {
+            @Override
+            public void LocationResult(Location location) {
+                showMyLocationOnUI(location);
+            }
+        };
+    }
+
+    void showMyLocationOnUI(Location location){
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+
+        Geocoder geocoder = new Geocoder(this);
+
+        try {
+            //get the address
+            List<Address> addresses = geocoder.getFromLocation(lat,lon,1);
+            String address = addresses.get(0).getAddressLine(0);
+
+            ((TextView)findViewById(R.id.GPSLocation)).setText(address);
+
+        } catch (IOException e) {
+            ((TextView)findViewById(R.id.GPSLocation)).setText("unable to get address");
+            e.printStackTrace();
+        }
+
+    }
 
     /**
      * runs when calendar button is clicked
@@ -52,7 +91,7 @@ public class PostATaskActivity extends AppCompatActivity
         return false;
     }
 
-    void spinnerSetup() {
+    public void spinnerSetup() {
         Spinner spinner = (Spinner) findViewById(R.id.tasksTypeSpinner_PostATask);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
@@ -64,7 +103,6 @@ public class PostATaskActivity extends AppCompatActivity
     }
 
 
-    int numberOfLastPostInDB = 0;
     Calendar expectedDate;
     public void postATaskOnButtonClick(View view){
         if(!areRequiredFieldsProvidedAndShowStatus()){
@@ -75,30 +113,7 @@ public class PostATaskActivity extends AppCompatActivity
         //path to database object
         String path = "users/"+ userId +"/TaskPosts";
 
-        //read data from database
-        DbRead<DataSnapshot> dbRead = new DbRead<DataSnapshot>(path,
-                DataSnapshot.class, db) {
-            @Override
-            public void getReturnedDbData(DataSnapshot dataFromDb) {
-                numberOfLastPostInDB = (int) dataFromDb.getChildrenCount();
-
-                //get last item number
-                String taskId = "";
-                for (DataSnapshot userdata : dataFromDb.getChildren()) {
-                    taskId = userdata.getKey();
-                }
-                if(taskId != ""){
-                    numberOfLastPostInDB = Integer.parseInt(taskId.split("_")[0]);
-                }else {
-                    numberOfLastPostInDB = 0;
-                }
-
-                writeTaskToDB();
-            }
-        };
-
-
-
+        writeTaskToDB();
     }
 
     private boolean areRequiredFieldsProvidedAndShowStatus(){
@@ -106,6 +121,8 @@ public class PostATaskActivity extends AppCompatActivity
                 .getSelectedItem().toString();
         String description =  ((TextView) findViewById(R.id.taskDescEditTxt)).getText().toString();
         String cost =  ((TextView) findViewById(R.id.costEditTxt)).getText().toString();
+
+        System.out.println(cost);
 
         if(taskTitle.equals(getResources().getString(R.string.selectATask)))
         {
@@ -141,11 +158,16 @@ public class PostATaskActivity extends AppCompatActivity
         }
 
         String userId = UserStatusData.getEmail(this).replace(".", ";");
-        //path where you want to write data to
-        numberOfLastPostInDB++;
-        String path =  "users/"+ userId +"/TaskPosts/" + numberOfLastPostInDB + "_" + taskTitle ;
 
+        //path where you want to write data to
+        String path =  "users/"+ userId +"/TaskPosts/"  ;
+
+        // Get the unique ID generated by a push()
+        String postId =  db.getReference(path).push().getKey();
+
+        path = path + postId;
         TaskPost taskPost = new TaskPost(taskTitle,description,cost);
+
         new DbWrite<TaskPost>(path,taskPost,db) {
             @Override
             public void writeResult(TaskPost userdata) {
@@ -163,5 +185,17 @@ public class PostATaskActivity extends AppCompatActivity
 
         String date = DateFormat.getDateInstance().format(expectedDate.getTime());
         ((Button)findViewById(R.id.datePickerPostAtask)).setText(date);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 99 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            getLocation();
+        }else {
+            Toast.makeText(this,"This app requires permissions to get Location data"
+                    ,Toast.LENGTH_SHORT).show();
+        }
     }
 }
