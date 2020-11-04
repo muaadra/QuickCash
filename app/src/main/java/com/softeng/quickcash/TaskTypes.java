c4fac2ee68a85be4dca9e58a089f08f457e01380
alrawhani <md961867@dal.ca>
2020-11-04 15:33:06 -0400

    bug fix: US-6

--- app/src/main/java/com/softeng/quickcash/TaskTypes.java
+++ app/src/main/java/com/softeng/quickcash/TaskTypes.java
@@ -1,7 +1,7 @@
 package com.softeng.quickcash;
 
 public class TaskTypes {
-    private final static String[] taskTypes = {"Select A Category"
+    private static String[] taskTypes = {"Select A Category"
             , "lawn mowing", "dog walking","baby sitting", "computer repairs"};
 
     public static String[] getTaskTypes() {
