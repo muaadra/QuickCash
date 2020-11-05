d8afd65aa65ccc0529450681d47001f5e45da01f
alrawhani <md961867@dal.ca>
2020-11-04 21:02:48 -0400

    junit test: US-15

--- app/src/test/java/com/softeng/quickcash/ListSortTest.java
+++ app/src/test/java/com/softeng/quickcash/ListSortTest.java
@@ -25,11 +25,35 @@ public class ListSortTest {
         posts.add(taskPost1);
         posts.add(taskPost2);
 
-        DistanceSort distanceSort = new DistanceSort(true);
+        DistanceSort distanceSort = new DistanceSort();
         Collections.sort(posts,distanceSort);
 
         assertEquals("id_1",posts.get(0).getPostId());
         assertEquals("id_0",posts.get(1).getPostId());
+    }
+
+    /**
+     * test if elements are sorted in Descending order
+     */
+    @Test
+    public void DistanceSortDescendingTest1() {
+        TaskPost taskPost1 = new TaskPost();
+        taskPost1.setPostId("id_0");
+        taskPost1.setDistance(8.3f);
+
+        TaskPost taskPost2 = new TaskPost();
+        taskPost2.setPostId("id_1");
+        taskPost2.setDistance(1f);
+
+        ArrayList<TaskPost> posts = new ArrayList<>();
+        posts.add(taskPost1);
+        posts.add(taskPost2);
+
+        DistanceSort distanceSort = new DistanceSort(false);
+        Collections.sort(posts,distanceSort);
+
+        assertEquals("id_0",posts.get(0).getPostId());
+        assertEquals("id_1",posts.get(1).getPostId());
     }
 
 }
