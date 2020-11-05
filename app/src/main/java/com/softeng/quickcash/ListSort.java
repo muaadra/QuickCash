023fc83861a65b8f8d468346074bbf9149aa7780
alrawhani <md961867@dal.ca>
2020-11-04 21:05:20 -0400

    feature: US-15

--- app/src/main/java/com/softeng/quickcash/ListSort.java
+++ app/src/main/java/com/softeng/quickcash/ListSort.java
@@ -6,12 +6,18 @@ public class ListSort {
 }
 class DistanceSort implements Comparator<TaskPost> {
 
-    public DistanceSort() {
+    private int ascending;
 
+    public DistanceSort(boolean ascending) {
+        if(ascending){
+            this.ascending = 1;
+        }else {
+            this.ascending = -1;
+        }
     }
 
     @Override
     public int compare(TaskPost t1, TaskPost t2) {
-        return Float.compare(t1.getDistance(), t2.getDistance()) ;
+        return Float.compare(t1.getDistance(), t2.getDistance()) * ascending;
     }
 }
