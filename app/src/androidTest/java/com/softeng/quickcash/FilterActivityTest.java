c27cecd8ac3479ea970b1553e6fe783cde609256
alrawhani <md961867@dal.ca>
2020-11-05 22:15:24 -0400

    bug fix: US-1

--- app/src/androidTest/java/com/softeng/quickcash/FilterActivityTest.java
+++ app/src/androidTest/java/com/softeng/quickcash/FilterActivityTest.java
@@ -17,20 +17,17 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
-import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class FilterActivityTest {
     @Rule
-    public ActivityScenarioRule<MainActivity> activityScenarioRule
-            = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<FilterActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(FilterActivity.class);
 
     /**
      * setup method for remove UserStatus Data.
@@ -38,31 +35,16 @@ public class FilterActivityTest {
     @Before
     public void setup() {
         activityScenarioRule.getScenario().onActivity(
-                new ActivityScenario.ActivityAction<MainActivity>() {
+                new ActivityScenario.ActivityAction<FilterActivity>() {
                     @Override
-                    public void perform(MainActivity activity) {
+                    public void perform(FilterActivity activity) {
                         UserStatusData.removeAllUserPreferences(activity);
-                        Intent intent = new Intent(activity, WelcomeActivity.class);
+                        Intent intent = new Intent(activity, FilterActivity.class);
                         activity.startActivity(intent);
                     }
                 });
-        onView(withId(R.id.continueAsGuestButton)).perform(click());
-        onView(withId(R.id.btn_filter)).perform(click());
     }
-    /**
-     * this Ui test if user click categories.
-     */
-    @Test
-    public void filter_categories_test() {
-        onView(withId(R.id.btn_categories_1)).perform(click())
-            .check(matches(isChecked()));
-        onView(withId(R.id.btn_categories_2)).perform(click())
-            .check(matches(isChecked()));
-        onView(withId(R.id.btn_categories_3)).perform(click())
-            .check(matches(isChecked()));
-        onView(withId(R.id.btn_categories_4)).perform(click())
-            .check(matches(isChecked()));
-    }
+
     /**
      * this Ui test if user change distance.
      */
@@ -71,18 +53,15 @@ public class FilterActivityTest {
         onView(withId(R.id.seekBar)).perform(new GeneralClickAction(Tap.SINGLE, GeneralLocation.CENTER_LEFT, Press.FINGER));
         onView(withId(R.id.distance_text)).check(matches(withText("0km")));
         onView(withId(R.id.seekBar)).perform(new GeneralClickAction(Tap.SINGLE, GeneralLocation.CENTER, Press.FINGER));
-        onView(withId(R.id.distance_text)).check(matches(withText("500km")));
-        onView(withId(R.id.seekBar)).perform(new GeneralClickAction(Tap.SINGLE, GeneralLocation.CENTER_RIGHT, Press.FINGER));
-        onView(withId(R.id.distance_text)).check(matches(withText("1000km")));
+        onView(withId(R.id.distance_text)).check(matches(withText((MainActivity.MAX_LOCAL_DISTANCE/2000) + "km")));
     }
     /**
      * this Ui test if user input correct number.
      */
     @Test
     public void filter_price_correct() {
-        onView(withId(R.id.price_minText)).perform(typeText("50"));
+        onView(withId(R.id.minPayTV)).perform(typeText("50"));
         onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.price_maxText)).perform(typeText("60"));
         onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
 
 
@@ -92,9 +71,8 @@ public class FilterActivityTest {
      */
     @Test
     public void filter_price_incorrect() {
-        onView(withId(R.id.price_minText)).perform(typeText("50"));
+        onView(withId(R.id.minPayTV)).perform(typeText("50"));
         onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
-        onView(withId(R.id.price_maxText)).perform(typeText("40"));
         onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
         onView(withId(R.id.err_msg_price)).check(matches(isDisplayed()));
     }
