e2c0fda1dfc0bebcd99d5c904ad8ef0df414d383
alrawhani <md961867@dal.ca>
2020-11-25 16:55:20 -0400

    feature: US-20

---
+++ app/src/main/java/com/softeng/quickcash/NewTasksNotifications.java
@@ -0,0 +1,18 @@
+package com.softeng.quickcash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class NewTasksNotifications extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_tasks_notifications);
+    }
+
+
+
+}
