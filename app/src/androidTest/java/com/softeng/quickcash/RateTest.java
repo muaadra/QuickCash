6904f0b70b4190774680e66121816688500db8f7
hpark <hn946002@dal.ca>
2020-11-26 14:39:40 -0400

    espresso test: US-17

---
+++ app/src/androidTest/java/com/softeng/quickcash/RateTest.java
@@ -0,0 +1,27 @@
+package com.softeng.quickcash;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class RateTest {
+
+    @Rule
+    public ActivityScenarioRule<Rate> activityScenarioRule
+            = new ActivityScenarioRule<>(Rate.class);
+
+
+    @Test
+    public void rateTest1() {
+        onView(withId(R.id.r1)).perform(click());
+        onView(withId(R.id.ratingLabel))
+                .check(matches(withText("1")));
+    }
+}
