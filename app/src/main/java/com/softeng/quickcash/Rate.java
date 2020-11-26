38bf80f96b0da7969d26f2913dc1815da1548be3
hpark <hn946002@dal.ca>
2020-11-26 14:55:09 -0400

    feature: US-17

--- app/src/main/java/com/softeng/quickcash/Rate.java
+++ app/src/main/java/com/softeng/quickcash/Rate.java
@@ -13,8 +13,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Rate extends AppCompatActivity {
     int rating;
-    ImageView stars[] = new ImageView[1];
-    String userIDToBeRated = "m@m;m";
+    ImageView stars[] = new ImageView[5];
+    String userIDToBeRated = "m@m;m"; ///**********place holder, keep it for now
     userProfile userToBeRatedProfile;
     String postID;
     final FirebaseDatabase db = FirebaseDatabase.getInstance();
@@ -25,6 +25,16 @@ public class Rate extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
         getPassedArgs();
+        //read data from database
+        String path = "users/"+ userIDToBeRated +"/Profile";
+
+        new DbRead<userProfile>(path, userProfile.class, db) {
+            @Override
+            public void getReturnedDbData(userProfile dataFromDb) {
+                userToBeRatedProfile = dataFromDb;
+                ((TextView)findViewById(R.id.userToBeRated)).setText(userToBeRatedProfile.getfName());
+            }
+        };
         findStarViews();
     }
 
@@ -39,7 +49,10 @@ public class Rate extends AppCompatActivity {
 
     private void findStarViews() {
         stars[0] = findViewById(R.id.r1);
-
+        stars[1] = findViewById(R.id.r2);
+        stars[2] = findViewById(R.id.r3);
+        stars[3] = findViewById(R.id.r4);
+        stars[4] = findViewById(R.id.r5);
     }
 
 
@@ -48,9 +61,33 @@ public class Rate extends AppCompatActivity {
         rating = Integer.parseInt(v.getTag().toString());
         ((TextView) findViewById(R.id.ratingLabel)).setText(rating + "");
 
+        //reset stars
+        for (int i = 0; i < stars.length ; i++) {
+            stars[i].setImageResource(R.drawable.rating_star_grey);
+        }
+
+        //set new rating
+        for (int i = 0; i < rating ; i++) {
+            stars[i].setImageResource(R.drawable.rating_star);
+        }
     }
 
+    public void submitRating(View v){
+        //delete application from employer side
+        String path = "users/"+ userIDToBeRated + "/Ratings/" +
+                postID;
 
+        new DbWrite<Integer>(path,rating,db) {
+            @Override
+            public void writeResult(Integer userdata) {
+                finishActivity();
+            }
+        };
+    }
 
+    private void finishActivity(){
+        Toast.makeText(this,"Rating submitted",Toast.LENGTH_SHORT).show();
+        finish();
+    }
 
 }
