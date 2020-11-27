4864ba430047d36aa7ca1e36b4941f8b4df14578
jrobichaud <jn692029@dal.ca>
2020-11-26 22:14:25 -0400

    espresso test: US-22

--- app/src/androidTest/java/com/softeng/quickcash/ApplicantsTest.java
+++ app/src/androidTest/java/com/softeng/quickcash/ApplicantsTest.java
@@ -1,5 +1,7 @@
 package com.softeng.quickcash;
 
+import android.util.Log;
+
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -37,5 +39,44 @@ public class ApplicantsTest {
                 });
 
         assertEquals(false, adapter[0]);
+    }
+
+    /*
+     * Test count of recycler view items
+     */
+    @Test
+    public void testApplicantRecyclerViewTest(){
+        final ArrayList<userProfile> profiles = new ArrayList<>();
+        userProfile up1 = new userProfile("Jonathan","about me!");
+        userProfile up2 = new userProfile("Robichaud", "my last name!");
+        profiles.add(up1);
+        profiles.add(up2);
+
+        final int[] count = {-1};
+
+
+        activityScenarioRule.getScenario().onActivity(
+                new ActivityScenario.ActivityAction<Applicants>() {
+                    @Override
+                    public void perform(Applicants activity) {
+                        activity.createRecyclerView(profiles);
+                        count[0] = (((RecyclerView) activity.findViewById(R.id.applicantsRecyclerView)).getAdapter().getItemCount());
+                    }
+                });
+
+        assertEquals(2,count[0]);
+
+        profiles.remove(1);
+        activityScenarioRule.getScenario().onActivity(
+                new ActivityScenario.ActivityAction<Applicants>() {
+                    @Override
+                    public void perform(Applicants activity) {
+                        activity.createRecyclerView(profiles);
+                        count[0] = (((RecyclerView) activity.findViewById(R.id.applicantsRecyclerView)).getAdapter().getItemCount());
+                    }
+                });
+
+        assertEquals(1,count[0]);
+
     }
 }
